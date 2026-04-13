93bdbe31b2d50d6654715a35cf404c904945cc8f
webdesignrahim4061 <webdesignrahim4061@gmail.com>
2026-04-13 10:42:08 +0600

    change hero sesction

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <div className='bg-[#D2D2D220]'>
             <div className="max-w-11/12 mx-auto pt-10">
                 <h1 className='text-4xl font-bold text-center bg-linear-to-br to-[#632EE3] from-[#9F62F2] bg-clip-text'>We Build <br />
-                    <span className='text-transparent'>Productive</span> Apps</h1>
+                    <span className='text-transparent'>Productive</span> Apps Github</h1>
 
                 <p className='text-center text-[#627382] max-w-[80%] mx-auto mt-3'>At HERO.IO, we craft innovative apps designed to make everyday life simpler, smarter, and more exciting.Our goal is to turn your ideas into digital experiences that truly make an impact.</p>
 
